d78565fb29f01947c1ceaf0bac08602880690925
Bruno Paixão <bruno.amordivino@gmail.com>
2025-07-29 20:58:42 -0300

    fix: add cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
-
+const cors = require("cors");
 const swaggerFile = require("../swagger-output.json");
 const producaoRoutes = require("./routes/producaoRoutes");
 const { connectDB } = require("./config/db");
@@ -11,8 +11,9 @@ const PORT = 3002;
 
 connectDB()
   .then(() => {
-    app.use(bodyParser.json());
+    app.use(cors());
 
+    app.use(bodyParser.json());
     // Rotas da API
     app.use("/pedidos", producaoRoutes);
 
@@ -20,7 +21,6 @@ connectDB()
     app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
     app.get("/", (req, res) => {
-      // #swagger.ignore = true
       res.send(
         "Microsserviço de Produção está no ar e conectado ao MongoDB! Acesse /doc para ver a documentação."
       );
